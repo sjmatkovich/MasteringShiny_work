116273369c3ee1396ca87e0ba36d6eae2f9c3fb2
Scot J Matkovich <sjmatkovich@live.com>
2024-05-20 22:03:59 -0400

    completed_ch1_exercises

---
+++ ch1_ex3/app.R
@@ -0,0 +1,17 @@
+library(shiny)
+
+ui <- fluidPage(
+  sliderInput("x", label = "If x is", min = 1, max = 50, value = 30),
+  "and y is",
+  sliderInput("y", label = "If x is", min = 1, max = 50, value = 30),
+  "then x times y is",
+  textOutput("product")
+)
+
+server <- function(input, output, session) {
+  output$product <- renderText({ 
+    input$x * input$y
+  })
+}
+
+shinyApp(ui, server)
