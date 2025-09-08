140d95827b03b11c74bea852af9c12356bd53899
Scot J Matkovich <sjmatkovich@live.com>
2025-09-07 20:52:15 -0400

    renv update

--- ch2-3-5_ex3/app.R
+++ ch2-3-5_ex3/app.R
@@ -6,7 +6,13 @@ ui <- fluidPage(
 )
 server <- function(input, output, session) {
   # output$table <- renderDataTable(mtcars, options = list(pageLength = 5, searching = FALSE, ordering = FALSE, info = FALSE)) # deprecated in shiny 1.8.1
-  output$table <- DT::renderDT(mtcars, options = list(pageLength = 5, searching = FALSE, ordering = FALSE, info = FALSE))
+  output$table <- DT::renderDT(mtcars,
+                               options = list(
+                                 pageLength = 5,
+                                 searching = FALSE,
+                                 ordering = FALSE,
+                                 info = FALSE
+                               ))
 }
 
 shinyApp(ui, server)
